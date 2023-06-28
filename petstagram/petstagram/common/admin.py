bfe9436a0953ce7b9917587c9e2ba306ad10385b
RebelShark <tpetkov93@gmail.com>
2023-06-28 13:56:10 +0300

    X

--- petstagram/petstagram/common/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from petstagram.common.models import Comment, Like
-
-admin.site.register(Comment)
-admin.site.register(Like)
--- petstagram/petstagram/utils/convert_b_to_mb.py
+++
@@ -1,2 +0,0 @@
-def convert_mb_to_b(mb_input):
-    return mb_input * 1024 * 1024
